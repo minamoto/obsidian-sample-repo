b8813babd24bb97b10ddf73afcac7f926ad33b80
Fusayuki Minamoto <fusayuki.minamoto@gmail.com>
2026-01-11 14:25:31 +0900

    vault backup: 2026-01-11 14:25:31

--- 10_Draft/build.bat
+++ 10_Draft/build.bat
@@ -3,4 +3,5 @@ set adoc=installing_openshift_local.adoc
 call asciidoctor %adoc%
 call asciidoctor-pdf -a pdf-theme=default-with-font-fallbacks -a scripts=cjk %adoc%
 call asciidoctor-epub3 %adoc%
+call asciidoctor --backend docbook %adoc%
 echo done
