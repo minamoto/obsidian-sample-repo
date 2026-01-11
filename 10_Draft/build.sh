e59a6849b8dd758e827bbbb27defffb82f8d8e8e
Fusayuki Minamoto <fusayuki.minamoto@gmail.com>
2026-01-11 14:31:18 +0900

    fix

--- 10_Draft/build.sh
+++ 10_Draft/build.sh
@@ -3,5 +3,5 @@ adoc=installing_openshift_local.adoc
 asciidoctor ${adoc} 
 asciidoctor-pdf -a pdf-theme=default-with-font-fallbacks -a scripts=cjk ${adoc} 
 asciidoctor-epub3 ${adoc}
-asciidoctor --backend docbook %adoc% 
+asciidoctor --backend docbook ${adoc}
 echo done
